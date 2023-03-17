b03ebf98691d0735f7aa16a625db1dfddf78cd96
Juliana Costa <lojablendibox@gmail.com>
2023-03-17 15:56:21 -0300

    exemplo video

--- layouts/partials/Header.js
+++ layouts/partials/Header.js
@@ -1,54 +1,11 @@
-import menu from "@config/menu.json";
-import { useHeaderContext } from "context/state";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+
 const Header = () => {
   // router
-  const router = useRouter();
-
-  //context
-  const { categories } = useHeaderContext();
-
-  //local state
-  const [openMenu, setOpenMenu] = useState(false);
-  const [navMenu, setNavMenu] = useState(
-    menu.main.map((item) => ({ ...item, type: "main" }))
-  );
-
-  useEffect(() => {
-    const matchRoute = menu.main.find((item) => item.url === router.asPath);
-    const navList = [...menu.main];
-
-    if (matchRoute) {
-      if (matchRoute.url === "/") {
-        const arr = categories.slice(0, 4);
-        navList.splice(1, 0, ...arr);
-        setNavMenu(navList);
-      } else {
-        setNavMenu(menu.main);
-      }
-    } else {
-      //if route not match
-      navList.splice(1, 0, ...categories);
-      navList.splice(navList.length - 2, menu.main.length - 1);
-      setNavMenu(navList);
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [router.asPath, menu.main, categories]);
 
   return (
     <>
-      <header className={`header pb-3`}>
-
-          <div className="container text-center mt-12">navbar </div>
-        <nav className="navbar container text-center md:text-left">
-
-        
-          {/* navbar toggler */}
-         
-          </nav>
+      <header className={`header mt-8  pb-3`}>
+             
 
   
       </header>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 
 import Base from "@layouts/Baseof";
-
+import Youtube from "@layouts/shortcodes/Youtube";
 
  function index({item}) {
 
@@ -8,11 +8,38 @@ import Base from "@layouts/Baseof";
 
     <Base>
       {/* profile */}
+      <div className="mx-auto text-center lg:col-8">
+
+<div class="notice warning relative rounded mt-0 ">
+<div class="absolute h-[30px] -top-[30px] z-10 left-0 p-3 flex items-center">
+<svg width="16px" height="16px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+<path fill="#fff" 
+d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"></path></svg>
+
+<p class="my-0 text-dark ml-1.5 capitalize ">teste de algo icrível! &nbsp;</p>
+
+
+
+<svg width="16px" height="16px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+<path fill="#fff" 
+d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"></path></svg>
+
+</div>
+
+
+</div>
+
+
+
+      </div>
       <div className="section">
+
         <div className="container">
           <div className="row">
             <div className="mx-auto text-center lg:col-8">
-   oi
+
+            <Youtube id="qJA2U-YMvkk" title="Play: Video" />
+   
             </div>
           </div>
         </div>
